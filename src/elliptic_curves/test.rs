4cf03a056ccccaad5fe0082f92d6c557f7a6e759
jetSett <joel.felderhoff@ens-lyon.fr>
2019-02-06 13:18:20 +0100

    elliptic curve test fix

--- src/elliptic_curves/test.rs
+++ src/elliptic_curves/test.rs
@@ -1,23 +1,26 @@
 use super::*;
 
+use crate::field::*;
 use crate::finite_fields::*;
 
-const P : Integer = 10169;
+pub type Integer = gmp::mpz::Mpz;
 
-declare_finite_field!(K, P, m10169);
+const P : u32 = 10169;
+
+declare_finite_field!(K, Integer, Integer::from(P), m10169);
 
 fn sample_point(ell : &EllipticCurve<K>) -> ProjKPoint<K>{
     if ell.is_reduced_weierstrass(){
         ell.sample_point()
     }else{
         let mut p = ProjKPoint::FinPoint(
-            K::new(0), K::new(0)
+            K::from_int(0), K::from_int(0)
         );
 
         while !(ell.is_on_curve(&p)) {
             p = ProjKPoint::FinPoint(
-                K::new(rand::random::<Integer>()%P),
-                K::new(rand::random::<Integer>()%P),
+                K::new(Integer::sample_uniform(&Integer::from(0), &Integer::from(P))),
+                K::new(Integer::sample_uniform(&Integer::from(0), &Integer::from(P))),
             );
         }
         p
@@ -26,21 +29,21 @@ fn sample_point(ell : &EllipticCurve<K>) -> ProjKPoint<K>{
 
 fn sample_elliptic_curve() -> EllipticCurve<K>{
     let mut ell = EllipticCurve::<K>{
-                a_1: K::from_int(rand::random::<Integer>()%P),
-                a_3: K::from_int(rand::random::<Integer>()%P),
+                a_1: K::new(Integer::sample_uniform(&Integer::from(0), &Integer::from(P))),
+                a_3: K::new(Integer::sample_uniform(&Integer::from(0), &Integer::from(P))),
 
-                a_2: K::from_int(rand::random::<Integer>()%P),
-                a_4: K::from_int(rand::random::<Integer>()%P),
-                a_6: K::from_int(rand::random::<Integer>()%P),
+                a_2: K::new(Integer::sample_uniform(&Integer::from(0), &Integer::from(P))),
+                a_4: K::new(Integer::sample_uniform(&Integer::from(0), &Integer::from(P))),
+                a_6: K::new(Integer::sample_uniform(&Integer::from(0), &Integer::from(P))),
                 };
-    while ell.discriminant() == K::new(0){
+    while ell.discriminant() == K::from_int(0){
         ell = EllipticCurve::<K>{
-                    a_1: K::from_int(rand::random::<Integer>()%P),
-                    a_3: K::from_int(rand::random::<Integer>()%P),
+                    a_1: K::new(Integer::sample_uniform(&Integer::from(0), &Integer::from(P))),
+                    a_3: K::new(Integer::sample_uniform(&Integer::from(0), &Integer::from(P))),
 
-                    a_2: K::from_int(rand::random::<Integer>()%P),
-                    a_4: K::from_int(rand::random::<Integer>()%P),
-                    a_6: K::from_int(rand::random::<Integer>()%P),
+                    a_2: K::new(Integer::sample_uniform(&Integer::from(0), &Integer::from(P))),
+                    a_4: K::new(Integer::sample_uniform(&Integer::from(0), &Integer::from(P))),
+                    a_6: K::new(Integer::sample_uniform(&Integer::from(0), &Integer::from(P))),
                     };
     }
     ell
@@ -48,8 +51,8 @@ fn sample_elliptic_curve() -> EllipticCurve<K>{
 
 #[test]
 fn test_discriminant_zero(){
-    assert_eq!(EllipticCurve::<K>::new_reduced_weierstrass(K::new(0), K::new(0)).discriminant(), 
-                K::new(0));
+    assert_eq!(EllipticCurve::<K>::new_reduced_weierstrass(K::from_int(0), K::from_int(0)).discriminant(), 
+                K::from_int(0));
 
     assert_eq!(EllipticCurve::<K>{
                 a_1: K::from_int(0),
@@ -60,7 +63,7 @@ fn test_discriminant_zero(){
                 a_6: K::from_int(0),
 
                 }.discriminant(), 
-                K::new(0));
+                K::from_int(0));
 }
 
 #[test]
@@ -82,44 +85,45 @@ fn j_invariant_constant_weierstrass() {
 
 #[test]
 fn point_addition_commut(){
-    let ell = sample_elliptic_curve();
+    let ell = sample_elliptic_curve().to_reduced_weierstrass();
     for _i in 1..10{
         let p1 = sample_point(&ell);
         let p2 = sample_point(&ell);
-        assert_eq!(ell.add_points(p1, p2), ell.add_points(p2, p1));
+        assert_eq!(ell.add_points(p1.clone(), p2.clone()), ell.add_points(p2, p1));
     }
 }
 
 #[test]
 fn point_addition_assoc(){
-    let ell = sample_elliptic_curve();
+    let ell = sample_elliptic_curve().to_reduced_weierstrass();
     for _i in 1..10{
         let p1 = sample_point(&ell);
         let p2 = sample_point(&ell);
         let p3 = sample_point(&ell);
-        assert_eq!(ell.add_points(p1, ell.add_points(p2, p3)), ell.add_points(ell.add_points(p1, p2), p3));
+        assert_eq!(ell.add_points(p1.clone(), ell.add_points(p2.clone(), p3.clone())), ell.add_points(ell.add_points(p1, p2), p3));
     }
 }
 
-fn trivial_scalar_mult(ell : &EllipticCurve<K>, n : Integer, point : ProjKPoint<K>) -> ProjKPoint<K>{
+fn trivial_scalar_mult(ell : &EllipticCurve<K>, n : i32, point : ProjKPoint<K>) -> ProjKPoint<K>{
     if n < 0 {
         return trivial_scalar_mult(ell, -n, ell.neg_point(point));
     }
 
     let mut p2 = ProjKPoint::InfPoint;
     for _i in 1..(n+1){
-        p2 = ell.add_points(p2, point);
+        p2 = ell.add_points(p2, point.clone());
     }
     p2
 }
 
 #[test]
 fn scalar_mult_correct() {
-    let ell = sample_elliptic_curve();
-    for _i in 1..10{
-        let n = rand::random::<Integer>() % 100;
+    let ell = sample_elliptic_curve().to_reduced_weierstrass();
+    for _i in 1..3{
+        let n_int = rand::random::<i32>()%100;
+        let n = Integer::from(n_int);
         let p = sample_point(&ell);
-        let p_trivial = trivial_scalar_mult(&ell, n, p);
+        let p_trivial = trivial_scalar_mult(&ell, n_int, p.clone());
         let p_ladder = ell.scalar_mult(n, p);
         assert_eq!(p_trivial, p_ladder);
     }
@@ -127,7 +131,7 @@ fn scalar_mult_correct() {
 
 #[test]
 fn velu_isogeny_is_isogeny(){
-    for _i in 1..10{
+    for _i in 1..3{
         let ell = sample_elliptic_curve().to_reduced_weierstrass();
         let p = sample_point(&ell);
 
@@ -137,21 +141,21 @@ fn velu_isogeny_is_isogeny(){
 
 #[test]
 fn velu_isogeny_ip_kernel(){
-    for _i in 1..10{
+    for _i in 1..3{
         let ell = sample_elliptic_curve().to_reduced_weierstrass();
         let p = sample_point(&ell);
 
-        for _j in 1..10{
-            let n = rand::random::<Integer>()%500;
+        for _j in 1..3{
+            let n = Integer::sample_uniform(&Integer::from(0), &Integer::from(500));
 
-            assert_eq!(ell.velu_projection(&p, ell.scalar_mult(n, p)), ProjKPoint::InfPoint);
+            assert_eq!(ell.velu_projection(&p, ell.scalar_mult(n, p.clone())), ProjKPoint::InfPoint);
         }
     }
 }
 
 #[test]
 fn velu_formula_match_isogeny(){
-    for _i in 1..10{
+    for _i in 1..3{
         let ell = sample_elliptic_curve().to_reduced_weierstrass();
         let p = sample_point(&ell);
 
