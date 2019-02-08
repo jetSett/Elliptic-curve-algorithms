b0acddaf03a7863c8b5c0126ff189662ef8381c9
jetSett <joel.felderhoff@ens-lyon.fr>
2019-02-08 15:55:37 +0100

    Code cleaning and commentaries

--- src/csidh.rs
+++ src/csidh.rs
@@ -95,34 +95,31 @@ fn isogeny(ell : &EllipticCurve<K>, point : &UnsignedProjPoint<K>, mut q : Unsig
     assert!(k>=Integer::from(3));
     assert!(&k%2 != Integer::from(0));
 
-    let p_normalized = point.clone().normalize();
     let p = point.clone();
-    q = q.normalize();
 
-
-    // we start at i = 2 because of special doubling case
-    let mut t = p_normalized.clone();  // t = [i]p will iterate over elements of <p>
+    let mut i = Integer::from(1);
+    
+    let mut t = p.clone();  // t = [i]p will iterate over elements of <p>
     let mut t_minus_1 = UnsignedProjPoint::infinite_point();
 
     let mut pi = UnsignedProjPoint::finite_point(K::from_int(1));
     let mut sigma = K::from_int(0);
+    
     let mut projection_x = K::from_int(1);
     let mut projection_z = K::from_int(1);
 
-    let mut i = Integer::from(1);
     while &Integer::from(2)*&i < k{
-        if t.x == K::from_int(0){ // point of order 2
+        if t.x == K::from_int(0){ // point of order 2, we abort
             return Err(());
         }
 
         pi.x *= t.x.clone();
-
         pi.z *= t.z.clone();
 
         sigma += t.x.clone()/t.z.clone() - t.z.clone()/t.x.clone();
 
-        projection_x *= &t.x*&q.x - t.z.clone();
-        projection_z *= &q.x*&t.z - t.x.clone();
+        projection_x *= &t.x*&q.x - &t.z*&q.z;
+        projection_z *= &q.x*&t.z - &t.x*&q.z;
 
         if i == Integer::from(1){
             let _temp = t.clone();
@@ -149,7 +146,7 @@ fn isogeny(ell : &EllipticCurve<K>, point : &UnsignedProjPoint<K>, mut q : Unsig
         EllipticCurve::new_montgomery(pi.x*(&ell.a_2 - &(K::from_int(3)*sigma))),
         UnsignedProjPoint{
                 x: q.x*projection_x,
-                z: projection_z
+                z: q.z*projection_z
             }
     ))
 
@@ -190,8 +187,7 @@ pub fn naive_class_group_action(inst : &CSIDHInstance, pk : PublicKey, sk : Secr
                 p_point = UnsignedProjPoint::finite_point(x.clone());
                 q_point = ell.scalar_mult_unsigned(p_plus_1.clone()/L[i].clone(), p_point);
             }
-            // println!("{} ({})", x, L[i]);
-            // println!("{} - {}", order_naive(&ell, &q_point), L[i]);
+
             let (ell_, _) = isogeny(&ell, &q_point, q_point.clone(), L[i].clone()).unwrap();
             ell = ell_;
         }
@@ -206,7 +202,7 @@ pub fn class_group_action(inst : &CSIDHInstance, pk : PublicKey, mut sk : Secret
     let N_PRIMES = &inst.n_primes;
     let mut finished_total : [bool; 2] = [false, false];
 
-    let mut k_sign : [Integer; 2]= [Integer::from(1), Integer::from(1)]; // 1 -> >= 0;  10-> <= 0
+    let mut k_sign : [Integer; 2]= [Integer::from(1), Integer::from(1)]; // 1 -> >= 0;  0-> <= 0
     let mut s_sign : [Vec<Integer>; 2] = [vec!(), vec!()];
     let mut e_sign : [Vec<i32>; 2] = [vec!(), vec!()];
     let mut finished_sign : [Vec<bool>; 2] = [vec!(), vec!()];
@@ -231,8 +227,8 @@ pub fn class_group_action(inst : &CSIDHInstance, pk : PublicKey, mut sk : Secret
     let mut ell = EllipticCurve::new_montgomery(pk);
 
     while !finished_total[0] || !finished_total[1] {
+        // Sample the point
         let x = K::new(Integer::sample_uniform(&Integer::from(0), &(P-Integer::from(1))));
-        // println!("{}", sum_abs);
         let compute_rhs = | x | {
             &( &(x*x)*x  + &((&ell.a_2)*x)*x )+ x 
         };
@@ -256,41 +252,37 @@ pub fn class_group_action(inst : &CSIDHInstance, pk : PublicKey, mut sk : Secret
         let mut q_point = ell.scalar_mult_unsigned((P.clone()+Integer::from(1))/k.clone(), uns_p);
 
         for j in 0..s_sign[sign_index].len(){
-            // let i = s_sign[sign_index].len()-1-j;
-            let i = j;
+            
+            // Trick from the original implementation, they started by the big primes
+            let i = s_sign[sign_index].len()-1-j;
+
             if finished_sign[sign_index][i]{
                 continue;
             }
             finished_total[sign_index] = false;
 
             let li : Integer = s_sign[sign_index][i].clone();
-            // println!("{} - {}", li, e_sign[sign_index][i]);
 
             let r_point = ell.scalar_mult_unsigned(k.clone()/(li.clone()), q_point.clone());
 
             if &r_point == &UnsignedProjPoint::infinite_point(){
-                // println!("Too low index");
+                // The point sampled had too low l-index
                 continue;
             }
 
             let (ell_, q_point_) = match isogeny(&ell, &r_point, q_point.clone(), li.clone()){
                 Err(()) => {
+                    // This should never happend
                     println!("Erreur");
                     continue;
                     },
                 Ok(pair) => pair
             };
 
-            // println!("Point of the correct order found: {}", li);
-
             ell = ell_;
             q_point = q_point_;
 
-            // assert!(is_supersingular(inst, &ell));
-            // println!("Toujours supersinguliere");
             e_sign[sign_index][i] -= s;
-
-            // println!("{}", e_sign[sign_index][i]);
 
             if e_sign[sign_index][i] == 0{
                 finished_sign[sign_index][i] = true;
